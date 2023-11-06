762c5c3773e7db35666f4152c3c48daba3da40ff
“vismitha25” <vismithapriyanka152325.gmail.com>
2023-11-06 15:42:21 +0530

    Second

--- test_calculator1.py
+++ test_calculator1.py
@@ -7,13 +7,13 @@ class CalculatorTest(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(20, self.calc.add(3, 7), "The addition is wrong")
+        self.assertEqual(10, self.calc.add(3, 7), "The addition is wrong")
 
     def test_subtract(self):
-        self.assertEqual(9, self.calc.subtract(15, 3), "Subtraction is wrong")
+        self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
 
     def test_multiply(self):
-        self.assertEqual(40, self.calc.multiply(5, 6), "Multiplication is wrong")
+        self.assertEqual(30, self.calc.multiply(5, 6), "Multiplication is wrong")
 
     def test_divide(self):
         self.assertEqual(3, self.calc.divide(6, 2), "Division is wrong")
